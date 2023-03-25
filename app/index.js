2caa7fd38bdf43d81510bc21063b4fa74d887d0b
Gabriel Pereira <118020840+Chase242@users.noreply.github.com>
2023-03-25 17:24:52 -0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -20,6 +20,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     //Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     app.use(cors());
     next();
 });
